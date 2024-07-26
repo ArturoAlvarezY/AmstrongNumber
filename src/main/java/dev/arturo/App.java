1286eef9e6cf00608eb08f594e312ae119a0169c
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-07-26 14:11:38 +0200

    add the App, finished exercise

--- src/main/java/dev/arturo/App.java
+++ src/main/java/dev/arturo/App.java
@@ -1,5 +1,7 @@
 package dev.arturo;
 
+import dev.arturo.models.AmstrongNumber;
+
 /**
  * Hello world!
  */
@@ -12,6 +14,10 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        AmstrongNumber amstrongNumber = new AmstrongNumber();
+
+        System.out.println(amstrongNumber.calcIfIsAmstrongNumber(371));
+        System.out.println(amstrongNumber.calcIfIsAmstrongNumber(777));
     }
 }
